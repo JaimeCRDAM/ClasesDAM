89d218b8191f5b994deb9de3e71189aea6d79356
Jaime <lnote@outlook.es>
2021-09-24 14:30:22 +0200

    ultima

---
+++ Entornos de desarrollo/Tema 1/Practicas.py
@@ -0,0 +1,7 @@
+print("Dime tu nombre")
+nombre = str(input())
+
+print("Dime tu edad")
+edad = int(input())
+fecha_nac = 2021-edad
+print("Tu nombre es {0} y tu edad {1}, y ademas naciste en el año {2}".format(nombre, edad, fecha_nac))
