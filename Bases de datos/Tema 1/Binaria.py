c0b3901af631a511360f32c85c9fbb52b1c36228
Jaime <lnote@outlook.es>
2021-09-28 14:20:21 +0200

    Ejercicios xml

--- Bases de datos/Tema 1/Binaria.py
+++ Bases de datos/Tema 1/Binaria.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
-Vector = np.arange(1000)
+import threading
+Vector = np.arange(10000000)
 print("Que numero, del 0 al {0}".format(len(Vector)))
 NumeroABuscar = input()
 
@@ -9,6 +10,7 @@ _Auxiliar2 = Vector[len(Vector)//2:]
 
 Loops = 0
 _time = time.time()
+#Binary search
 while True:
     if _Auxiliar2[0] == int(NumeroABuscar) or _Auxiliar1[0] == int(NumeroABuscar):
         print("Binaria: Se ha encontrado el numero {0}, en {1} Loops, {2} segundos".format(
@@ -23,10 +25,27 @@ while True:
         _Auxiliar2 = _Auxiliar2[len(_Auxiliar2)//2:]
     Loops += 1
 
+Loops = 0
 _time = time.time()
+#Linear search
 for numero in Vector:
     if numero == int(NumeroABuscar):
         print("Secuencial: Se ha encontrado el numero {0}, en {1} Loops, {2} segundos".format(
             NumeroABuscar, Loops, time.time()-_time))
         break
     Loops += 1
+
+Loops = 0
+_time = time.time()
+#Back&Front search
+Front = 0
+Back = len(Vector)-1
+while Front <= Back:
+    if Vector[Front] == int(NumeroABuscar) or Vector[Back] == int(NumeroABuscar):
+        print("Back&Front: Se ha encontrado el numero {0}, en {1} Loops, {2} segundos".format(
+            NumeroABuscar, Loops, time.time()-_time))
+        break
+    Front += 1
+    Back -= 1
+    Loops += 1
+
