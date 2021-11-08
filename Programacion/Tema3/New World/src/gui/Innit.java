08a66a50a63ee555a8bf0b234f6bd9ffd650df6f
Jaime <lnote@outlook.es>
2021-11-08 14:30:28 +0100

    java

--- Programacion/Tema3/New World/src/gui/Innit.java
+++ Programacion/Tema3/New World/src/gui/Innit.java
@@ -1,12 +1,39 @@
 package gui;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 public class Innit {   
     public static void main() {      
-        JFrame frame = new JFrame("Mi primera GUI");      
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
-        frame.setSize(300, 300);      
-        JButton Button = new JButton("Presionar");      
-        frame.getContentPane().add(Button); // Agrega el botón al panel de contenido del marco     
-        frame.setVisible(true);   
+        JFrame frame1 = new JFrame("Mi primera GUI");
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        frame1.setSize(300, 300);
+
+        JPanel pannel1 = new JPanel();
+        JTextArea text = new JTextArea("Me Cago en tu puta madre");
+        pannel1.add(text);
+        frame1.add(pannel1);
+
+
+
+        JPanel ButtonPannel  = new JPanel();
+        ButtonPannel .setLayout(new GridLayout(0, 5));
+        frame1.add(ButtonPannel, BorderLayout.PAGE_END);
+
+        JButton Button1 = new JButton("Presionar");
+        ButtonPannel.add(Button1);
+
+        JButton Button2= new JButton("Presionar");
+        ButtonPannel.add(Button2);
+
+        JButton Button3= new JButton("Presionar");
+        ButtonPannel.add(Button3);
+
+        JButton Button4= new JButton("Presionar");
+        ButtonPannel.add(Button4);
+
+        JButton Button5= new JButton("Presionar");
+        ButtonPannel.add(Button5);
+        
+        frame1.setVisible(true);
     }
 }
